6aa84cb3575dada752569e3227e1d50931146680
Pedro <peedrofranq96@gmail.com>
2023-01-20 19:26:01 -0300

    Adicionando endpoint para registrar usuario

---
+++ src/Backend/LivroReceitas.API/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+using LivroReceitas.Application.UseCases.Usuario.Registrar;
+using LivroReceitas.Comunicacao.Requesicoes;
+using LivroReceitas.Comunicacao.Respostas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivroReceitas.API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UsuarioController : ControllerBase
+	{
+	
+		[HttpPost]
+		[ProducesResponseType(typeof(RespostaUsuarioRegistradoJson),StatusCodes.Status201Created)]
+		public async Task<IActionResult> RegistrarUsuario([FromServices]IRegistrarUsuarioUseCase useCase,
+			[FromBody] RequisicaoRegistrarUsuario request)
+		{
+			var resultado = await useCase.Executar(request);
+
+			return Created(string.Empty, resultado);
+		}
+	}
+}
