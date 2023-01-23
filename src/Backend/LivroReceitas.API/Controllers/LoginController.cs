93c03b578b0833d122671d6386df22e31b0e1717
Pedro <peedrofranq96@gmail.com>
2023-01-23 15:21:24 -0300

    adicionando o endpoint de login ao projeto

---
+++ src/Backend/LivroReceitas.API/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using LivroReceitas.Application.UseCases.Login.FazerLogin;
+using LivroReceitas.Comunicacao.Requesicoes;
+using LivroReceitas.Comunicacao.Respostas;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivroReceitas.API.Controllers
+{
+	public class LoginController : LivroReceitasController
+	{
+		[HttpPost]
+		[ProducesResponseType(typeof(RespostaLoginJson), StatusCodes.Status200OK)]
+		public async Task<IActionResult> Login([FromServices] ILoginUseCase useCase,[FromBody] RequisicaoLoginJson requisicao)
+		{
+			var resposta = await useCase.Executar(requisicao);
+			return Ok(resposta);
+		}
+	}
+}
