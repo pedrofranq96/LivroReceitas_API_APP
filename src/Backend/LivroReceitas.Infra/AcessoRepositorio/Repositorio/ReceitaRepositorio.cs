a05851d45b5af27595ecd2b9785c012cf9393d68
Pedro <peedrofranq96@gmail.com>
2023-01-26 01:54:28 -0300

    feat: criando a classe repositorio atualizar receita

--- src/Backend/LivroReceitas.Infra/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
+++ src/Backend/LivroReceitas.Infra/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
@@ -3,7 +3,7 @@ using LivroReceitas.Domain.Repositorio.Receita;
 using Microsoft.EntityFrameworkCore;
 
 namespace LivroReceitas.Infra.AcessoRepositorio.Repositorio;
-public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnlyRepositorio
+public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnlyRepositorio, IReceitaUpdateOnlyRepositorio
 {
 	private readonly Context _context;
 
@@ -12,9 +12,14 @@ public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnly
 		_context = context;
 	}
 
-	public async Task<Receita> RecuperarPorId(long receitaId)
+	 async Task<Receita> IReceitaReadOnlyRepositorio.RecuperarPorId(long receitaId)
 	{
 		return await _context.Receitas.AsNoTracking().Include(r=> r.Ingredientes).FirstOrDefaultAsync(r=> r.Id == receitaId);
+	}
+
+	 async Task<Receita> IReceitaUpdateOnlyRepositorio.RecuperarPorId(long receitaId)
+	{
+		return await _context.Receitas.Include(r=> r.Ingredientes).FirstOrDefaultAsync(r=> r.Id == receitaId);
 	}
 
 	public async Task<IList<Receita>> RecuperarTodasDoUsuario(long usuarioId)
@@ -25,5 +30,10 @@ public class ReceitaRepositorio : IReceitaWriteOnlyRepositorio, IReceitaReadOnly
 	public async Task Registrar(Receita receita)
 	{
 		await _context.Receitas.AddAsync(receita);
+	}
+
+	public void Update(Receita receita)
+	{
+		_context.Update(receita);
 	}
 }
